0dc672b21866530ff898c3d246306e562c87e1bb
txsabrina <stx.sabrina@gmail.com>
2023-08-17 14:33:21 -0300

    feat: Create cart page

---
+++ src/app/carrinho.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { IProdutoCarrinho } from './produtos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarrinhoService {
+  itens: IProdutoCarrinho[] = [];
+
+  constructor() { }
+
+  obtemCarrinho() {
+    this.itens = JSON.parse(localStorage.getItem("carrinho") || "[]");
+    return this.itens;
+  }
+
+  adicionarCarrinho(produto: IProdutoCarrinho) {
+    this.itens.push(produto);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+
+  removerProduto(id: number) {
+    this.itens = this.itens.filter(item => item.id !== id);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+
+  limparCarrinho() {
+    this.itens = [];
+    localStorage.clear();
+  }
+
+}
--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CarrinhoService } from 'src/app/carrinho.service';
 import { NotificacaoService } from 'src/app/notificao.service';
-import { IProduto } from 'src/app/produtos';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
 import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
@@ -17,7 +18,8 @@ export class DetalhesProdutoComponent {
   constructor(
     private produtosService: ProdutosService, 
     private route: ActivatedRoute,
-    private notificacaoService: NotificacaoService
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService
     ) {}
 
   ngOnInit(): void {
@@ -28,5 +30,10 @@ export class DetalhesProdutoComponent {
 
   adicionarAoCarrinho() {
     this.notificacaoService.notificar("O produto foi adicionado ao carrinho");
+    const produto: IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+    }
+    this.carrinhoService.adicionarCarrinho(produto)
   }
 }
